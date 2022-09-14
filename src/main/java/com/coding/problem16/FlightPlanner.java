6c1a0bd92fde9afe53090bbecd645d13bbc90017
eu82lw <adrian.cosma@ing.com>
2022-09-14 22:33:11 +0300

    Correct problem statement

--- src/main/java/com/coding/problem16/FlightPlanner.java
+++ src/main/java/com/coding/problem16/FlightPlanner.java
@@ -17,14 +17,14 @@ package com.coding.problem16;
  * with at most <code>k</code> stops. If there is no such route, return -1. *
  * <p>
  * <br/>
- * Example 1: Input: n = 4, flights = [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], src = 0,
- * dst = 3, k = 1 Output: 700 Explanation: The optimal path with at most 1 stop from city 0 to 3 is
- * marked in red and has cost 100 + 600 = 700. Note that the path through cities [0,1,2,3] is
+ * Example 1: Input: numberOfCities = 4, flightPrices = [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], src = 0,
+ * dst = 3, maxStops = 1 Output: 700 Explanation: The optimal path with at most 1 stop from city 0 to 3
+ * has cost 100 + 600 = 700. Note that the path through cities [0,1,2,3] is
  * cheaper but is invalid because it uses 2 stops.
  * <p>
- * Example 2: Input: n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 1
- * Output: 200 Explanation: The graph is shown above. The optimal path with at most 1 stop from city
- * 0 to 2 is marked in red and has cost 100 + 100 = 200.
+ * Example 2: Input: numberOfCities = 3, flightPrices = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, maxStops = 1
+ * Output: 200 Explanation: The optimal path with at most 1 stop from city
+ * 0 to 2 has cost 100 + 100 = 200.
  */
 public class FlightPlanner {
 
